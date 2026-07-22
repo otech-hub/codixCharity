041cda8f301ecb4f844cb87d31355910c1104a45
Ayomiposy <ayomiposi.joshua@preferreddigitalbusiness.com>
2026-07-22 16:11:18 +0100

    Connected Resend to the contact form, for email delivery and reset form data

--- api/contact.js
+++ api/contact.js
@@ -1,4 +1,3 @@
-import { data } from "autoprefixer";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -16,7 +15,7 @@ export default async function handler(req, res) {
 
   //   Use resend to send the message
   try {
-    const data = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: "info@codixcharityfoundation.org",
       to: "georgejoshuaayomiposi@gmail.com",
       subject: "Contact Us Response - CodixCharity",
@@ -30,11 +29,11 @@ export default async function handler(req, res) {
 
     // Handling Resend Error, incase it fails from resend
 
-    // if (error) {
-    //   return res.status(400).json({
-    //     message: error.message,
-    //   });
-    // }
+    if (error) {
+      return res.status(400).json({
+        message: error.message,
+      });
+    }
 
     return res.status(200).json({
       message: "Message sent successfully",
